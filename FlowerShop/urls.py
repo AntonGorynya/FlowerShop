b8af1d5aa2a0ee24bbdd97b61c11dccca8b93351
Anton <antongorynya1@gmail.com>
2023-08-23 11:16:04 +0300

    updates url and view

--- FlowerShop/urls.py
+++ FlowerShop/urls.py
@@ -22,4 +22,10 @@ urlpatterns = [
     path('', views.index, name='indexpage'),
     path('card/', views.card, name='card'),
     path('catalog/', views.catalog, name='catalog'),
+    path('consultation/', views.consultation, name='consultation'),
+    path('order/', views.order, name='order'),
+    path('order-step/', views.order, name='order-step'),
+    path('quiz/', views.quiz, name='quiz'),
+    path('quiz-step/', views.quiz_step, name='quiz-step'),
+    path('result/', views.result, name='result'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -15,3 +15,27 @@ def catalog(request):
     template = loader.get_template('catalog.html')
     return HttpResponse(template.render({}))
 
+
+def consultation(request):
+    template = loader.get_template('consultation.html')
+    return HttpResponse(template.render({}))
+
+
+def order(request):
+    template = loader.get_template('order.html')
+    return HttpResponse(template.render({}))
+
+
+def quiz(request):
+    template = loader.get_template('quiz.html')
+    return HttpResponse(template.render({}))
+
+
+def quiz_step(request):
+    template = loader.get_template('quiz-step.html')
+    return HttpResponse(template.render({}))
+
+
+def result(request):
+    template = loader.get_template('result.html')
+    return HttpResponse(template.render({}))
