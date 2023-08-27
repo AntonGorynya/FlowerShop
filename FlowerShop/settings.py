2ff2c6a816490bea0c87d3850add503d07cfa8eb
kaser137 <murilon375@gmail.com>
2023-08-27 19:46:05 +0300

    correct payment

--- FlowerShop/settings.py
+++ FlowerShop/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sfe9#(9h!v78k15s1so208db*355icjkp3^)7hz98wwc2npn$b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kaser137.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
