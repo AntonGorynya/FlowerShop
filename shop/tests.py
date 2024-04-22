439c076f5fbed86802d172b6e152e62a7b8300ec
Anton <antongorynya1@gmail.com>
2024-04-22 14:46:45 +0300

    add simple tests

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+@pytest.mark.parametrize('username,email,password', [('john', 'test@test.com', 'pass')])
+@pytest.mark.django_db(transaction=True)
+def test_add_user(username, email, password):
+    user = User.objects.create_user(
+        username=username,
+        email=email,
+        password=password
+    )
+    User.objects.get(id=user.id)
+
+
+@pytest.mark.parametrize('user_id', [1, 2])
+@pytest.mark.django_db(transaction=True)
+def test_raise_exception(user_id):
+    with pytest.raises(User.DoesNotExist):
+        User.objects.get(id=user_id)
